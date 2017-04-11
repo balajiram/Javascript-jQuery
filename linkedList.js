4a0c0032031dfb33336099126308ae8554753db0
BALAJI <balajikevin@yahoo.com>
2017-04-11 17:01:11 -0500

    Update linkedList.js
    Changes to sort the singly linked list usng mergeSort

--- linkedList.js
+++ linkedList.js
@@ -1,99 +1,134 @@
-var fn=(function(){
+/**
+* sort the single link list using mergesort
+*
+*/
 
-return {
+function LinkedList(){
+	this.head = null;
+}
 
-Node: function(data){
- return { data: data, next: null }
-},
-createLinkList(list){
- var linkList, node;
-  list.forEach(function(item, index){
-		if(node) {
-		 node.next = this.Node(item);
-		 node = node.next;
+
+function Node(data){
+	this.data = data;
+	this.next = null;
+}
+
+LinkedList.prototype.push = function(data){
+	
+	if(!this.head){
+		this.head = new Node(data);
+	}
+	else {//iterating to the end
+		var current = this.head;
+		while(current && current.next){
+			current = current.next;
 		}
-		else {
-		 linkList = this.Node(item);
-		 node = linkList;
+		current.next = new Node(data);
+	}
+}
+
+LinkedList.prototype.print = function(){
+	var current = this.head;
+	while(current){
+		console.log(current.data);
+		current = current.next;
+	}
+}
+
+function Merge(list1, list2){
+	
+	var mergedList = new LinkedList();
+	var leftList = list1.head;
+	var rightList = list2.head;
+	
+	/**
+	*@desc: method to copy the node data and move the point to next location
+	*/
+	var clone(node){
+		mergedList.push(node.data);
+		node = node.next;
+	}
+	
+	while(leftList && rightList){
+		if(leftList.data < rightList.data){
+			clone(leftList);
 		}
-	}, this);  
-
-return linkList;
-},
-
-printList: function(list){
- while(list !== null){
-  console.log(list.data);
-  list = list.next;
- }
-},
-
-removeDuplicate: function(list){
- var staticRef = list;
-  
-  while(staticRef !== null){
-
-   var movingRef = staticRef ;
-   while(movingRef !== null){
-     if(movingRef.next && movingRef.next.data === staticRef.data){
-      movingRef.next = movingRef.next.next;
-     }
-     movingRef = movingRef.next;
-   }
-  staticRef = staticRef.next;
-  }
- return list;
-},
-
-listPartition(list, startRef, backRef){
-
-  if(startRef == backRef){
-    return {
-       headRef: startRef,
-       backRef: backRef,
-       list: list 
-    };
-  }
-  var slowRef = startRef;
-  var fastRef = startRef;
-
-   while(fastRef !== backRef){
-    if(fastRef.next && fastRef.next.next){
-       fastRef = fastRef.next.next;
-       slowRef = slowRef.next;
-    }
-    else if(fase){}
-    else{
-     fastRef = null;
-    }
-   }
-   return {
-    headRef: list,
-    backRef: slowRef,
-    list: list 
-   };
-},
-
-mergeSort: function(list,start,end){
-
-
-while(start !== end){
-   var response = this.listPartition(list,start,end);
-   
-   start = headRef;
-   end = backRef;
-
-  this.mergeSort(list,end.next,end
+		else{
+			clone(rightList);
+		}
+	}
+	while(leftList){
+		clone(leftList);
+	}
+	while(rightList){
+		clone(rightList);
+	}
+	
+	return mergedList;
 }
 
-console.log(response);
- return response;
+function MergeSort(list) {
+	
+	var partitionList = function(list){
+		
+		var slowPtr,fastPtr;
+		//empty linked list
+		if(!list || !list.head){
+			return {left: null, right: null};
+		}
+		
+		slowPtr = list.head;
+		fastPtr = list.head;
+		
+		var leftList = new LinkedList();
+		var rightList = new LinkedList();
+		
+		while(fastPtr.next){
+			if(fastPtr.next && fastPtr.next.next){
+				 //copying the data
+			    leftList.push(slowPtr.data);
+			   
+				fastPtr = fastPtr.next.next;
+				slowPtr = slowPtr.next;
+			}
+			else{
+				//copying the data
+			    leftList.push(slowPtr.data);
+				slowPtr = slowPtr.next;
+				fastPtr = fastPtr.next;
+			}
+		}
+		
+		while(slowPtr){
+			rightList.push(slowPtr.data);
+			slowPtr = slowPtr.next;
+		}
+		
+		return response ={ left: leftList, right: rightList };
+	}
+	
+    //empty list/ list with one node already sorted
+	if(!list && !list.head && !list.head.next){
+		return list;
+	}
+	else {
+		
+		var leftList = null, rightList = null;
+		
+		var partition = partitionList(list);
+		
+		leftList = partition.left;
+		rightList = partition.right;
+		
+		if(leftList && leftList.head.next){
+		  leftList = MergeSort(leftList);
+		}
+		
+		if(rightList && rightList.head.next){
+			rightList = MergeSort(rightList);
+		}
+	    
+		return Merge(leftList, rightList);
+	}
+	
 }
-}
-})();
-
-
-var list = fn.createLinkList([6,4,21,6,21]);
-
-fn.mergeSort(list,list,null);
-
